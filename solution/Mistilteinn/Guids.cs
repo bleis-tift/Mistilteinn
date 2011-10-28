3d5436fba681dfc9d99ef59d7a3182ca5ddb43bc
bleis-tift <bleis-tift@hotmail.co.jp>
2011-10-28 23:59:10 +0900

    first commit

---
+++ solution/Mistilteinn/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Mistilteinn
+{
+    static class GuidList
+    {
+        public const string guidMistilteinnPkgString = "060f1399-0689-4205-9ee8-9c1cffcdbda0";
+        public const string guidMistilteinnCmdSetString = "26295019-2819-45b0-9304-fdf5ddce357b";
+
+        public static readonly Guid guidMistilteinnCmdSet = new Guid(guidMistilteinnCmdSetString);
+    };
+}
---
+++ solution/Mistilteinn/PkgCmdID.cs
@@ -0,0 +1,15 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace Mistilteinn
+{
+    static class PkgCmdIDList
+    {
+        public const int cmdidFixup = 0x0101;
+        public const int cmdidMasterize = 0x0102;
+        public const int cmdidTicketList = 0x0103;
+        public const int cmdidPrivateBuild = 0x0104;
+        public const int cmdidPull = 0x0105;
+    };
+}
